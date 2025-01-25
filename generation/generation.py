aa4a6c2ecc2e2bf14a9f430a7d7585e2ec9a9294
hekpa <hekpacobjun@gmail.com>
2025-01-25 23:02:25 +0400

    Красивый вывод + пофикшенный выход за границы уровня

--- generation/generation.py
+++ generation/generation.py
@@ -1,82 +1,102 @@
 import random
-# weight, hight = map(int, input().split())
-weight, hight = 64, 64
-a = 3
-lst = ['0'] * hight
-for i in range(hight):
-    lst[i] = [' '] * weight
-k = random.randint(0, hight - 1)
-k1 = random.randint(0, hight - 1)
-lst[k][0] = '*'
-lst[k1][-1] = '*'
+
+width, height = 64, 64
+level = ['0'] * height
+
+for i in range(height):
+    level[i] = ['□'] * width
+
+start = random.randint(0, height - 1)
+level[start][0] = '*'
 ind1 = 0
-ind2 = k
-sp = ['d', 'd', 'u', 'u', 'd', 'u', 'r', 'r', 'r', 'r']
-sp1 = ['d', 'd', 'd', 'l', 'l', 'l', 'r', 'r', 'r', 'r']
-sp2 = ['d', 'd', 'd', 'r', 'l', 'l', 'l', 'l', 'r', 'r']
-last  = ''
+ind2 = start
+sp = ['U'] * 5 + ['D'] * 5 + ['R'] * 6
+sp1 = [['U'] * 20 + ['D'] * 4 + ['R'] * 4 + ['L'] * 4,
+       ['U'] * 20 + ['D'] * 4 + ['R'] * 4 + ['L'] * 4,
+       ['U'] * 4 + ['D'] * 20 + ['R'] * 6 + ['L'] * 4,
+       ['U'] * 4 + ['D'] * 20 + ['R'] * 4 + ['L'] * 6]
 
-while ind1 != weight -1:
+last = ''
+path = []
+while ind1 != width - 1:
     r = random.choice(sp)
-    if r == 'r':
+    if r == 'R':
         ind1 += 1
-        lst[ind2][ind1] = '+'
+        level[ind2][ind1] = '■'
+        path.append((ind2, ind1))
         last = ''
-    elif r == 'd' and ind2 != hight - 1 and last != 'u':
+    elif r == 'D' and ind2 != height - 1 and last != 'U':
         ind2 += 1
-        lst[ind2][ind1] = '+'
-        last = 'd'
-    elif r == 'u' and ind2 != 0 and last != 'd':
+        level[ind2][ind1] = '■'
+        path.append((ind2, ind1))
+        last = 'D'
+    elif r == 'U' and ind2 != 0 and last != 'D':
         ind2 -= 1
-        lst[ind2][ind1] = '+'
-        last = 'u'
+        level[ind2][ind1] = '■'
+        path.append((ind2, ind1))
+        last = 'U'
 
-if ind2 < k1:
-    for i in range(ind2 + 1, k1):
-        lst[i][-1] = '+'
-elif ind2 > k1:
-    for i in range(k1 + 1, ind2 + 1):
-        lst[i][-1] = '+'
+del path[-1]
+sub_paths = []
 
+level[ind2][ind1] = '*'
 
-i1 = random.randint(0 , weight // 2)
-i2 = random.randint(weight // 2 + 1, weight - 1)
+for i in range(64):
+    if i > 5:
+        start = random.choice([random.choice(path),
+                               random.choice(sub_paths),
+                               random.choice(sub_paths),
+                               random.choice(sub_paths),
+                               random.choice(sub_paths),
+                               random.choice(sub_paths),
+                               random.choice(sub_paths),
+                               random.choice(sub_paths),
+                               random.choice(sub_paths),
+                               random.choice(sub_paths)])
+    else:
+        start = random.choice(path)
 
-ind1 = i1
-ind2 = 0
-while ind2 != hight -1:
-    r = random.choice(sp1)
-    if r == 'r' and ind1 != weight - 2 and last != 'l':
-        ind1 += 1
-        lst[ind2][ind1] = '+'
-        last = 'r'
-    elif r == 'd':
-        ind2 += 1
-        lst[ind2][ind1] = '+'
-        last = ''
-    elif r == 'l' and ind1 != 1 and last != 'r':
-        ind1 -= 1
-        lst[ind2][ind1] = '+'
-        last = 'l'
+    ind2, ind1 = start
 
+    cur_sp = random.choice(sp1)
 
-ind1 = i2
-ind2 = 0
-while ind2 != hight -1:
-    r = random.choice(sp2)
-    if r == 'r' and ind1 != weight - 2 and last != 'l':
-        ind1 += 1
-        lst[ind2][ind1] = '+'
-        last = 'r'
-    elif r == 'd':
-        ind2 += 1
-        lst[ind2][ind1] = '+'
-        last = ''
-    elif r == 'l' and ind1 != 1 and last != 'r':
-        ind1 -= 1
-        lst[ind2][ind1] = '+'
-        last = 'l'
+    for j in range(random.randint(32, 64)):
+        r = random.choice(cur_sp)
+        if r == 'R' and last != 'L' and ind1 <= width - 2:
+            ind1 += 1
+            level[ind2][ind1] = '■'
+            sub_paths.append((ind2, ind1))
+            last = 'R'
+        elif r == 'L' and last != 'R' and ind1 >= 1:
+            ind1 -= 1
+            level[ind2][ind1] = '■'
+            sub_paths.append((ind2, ind1))
+            last = 'L'
+        elif r == 'D' and ind2 <= height - 2 and last != 'U':
+            ind2 += 1
+            level[ind2][ind1] = '■'
+            sub_paths.append((ind2, ind1))
+            last = 'D'
+        elif r == 'U' and ind2 >= 1 and last != 'D':
+            ind2 -= 1
+            level[ind2][ind1] = '■'
+            sub_paths.append((ind2, ind1))
+            last = 'U'
 
+'''
+# multiplication
+large_level = []
+for i in level:
+ string = ''
+ for j in i:
+  string += j * 2
+ large_level.append(string)
+ large_level.append(string)
 
-for i in range(len(lst)):
-    print(lst[i])
+print(*large_level, sep='\n')
+'''
+
+# print(path)
+for i in level:
+    print(''.join(i))
+print(len(level))
