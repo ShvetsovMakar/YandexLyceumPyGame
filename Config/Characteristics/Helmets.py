7a9d6f9da16f14b078e62cfd6b926c73ce0a324a
ShvetsovMakar <shved.mash@outlook.com>
2025-03-01 18:44:41 +0300

    deleted extra gear elements

--- Config/Characteristics/Helmets.py
+++ Config/Characteristics/Helmets.py
@@ -2,10 +2,6 @@ HELMETS = {
     "Falcon Helmet": {
         "protection": 20,
         "agility": 10
-    },
-    "Berserker Helmet": {
-        "protection": 40,
-        "agility": 0
     }
 }
 
--- Config/Characteristics/Weapons.py
+++ Config/Characteristics/Weapons.py
@@ -3,14 +3,6 @@ WEAPONS = {
         "damage": 100,
         "hits": 1
     },
-    "Blades Of Chaos": {
-        "damage": 55,
-        "hits": 2
-    },
-    "Falcon Saber": {
-        "damage": 40,
-        "hits": 3
-    },
     "Wooden Sword": {
         "damage": 60,
         "hits": 1
